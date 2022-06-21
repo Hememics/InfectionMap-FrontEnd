e339caaa5a23218f0ea4e4fb954a22c8e573833b
Carl Brando <carllb52@gmail.com>
2022-06-21 17:00:18 -0400

    stylized login and register pages

--- src/views/NavBar.tsx
+++ src/views/NavBar.tsx
@@ -2,14 +2,15 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 const logo = require('../assets/HeMemics_Logo.png');
+const videoBg = require('../assets/HememicsVideoBackground.mp4');
 
 const NavBar = () => {
 return (  <div>
             
             <div>
-                <video autoPlay loop muted id="video">
+                <video autoPlay loop muted id="video" >
                 
-                    <source src="https://hememics.com/wp-content/uploads/2020/04/HememicsVideoBackground-av02.mp4" type='video/mp4' />
+                    <source src={videoBg} type='video/mp4' />
                 </video>
                 <a href="https://hememics.com" >
                     <img src={logo} alt="hememics logo" className="logo"/>
