909d3750fc80cc881cf5683a594bdd2254b00b81
ankitkr177 <ankitkr177@gmail.com>
2022-07-21 11:48:28 -0400

    Using https://hememicsapi.com/api/ as the API server.

--- src/config/constant.ts
+++ src/config/constant.ts
@@ -1,7 +1,8 @@
 const BASENAME = '';
 const BASE_URL = '/app/dashboard/default';
 const BASE_TITLE = ' | React Datta Able ';
-const API_SERVER = 'http://flask-env.eba-2cgkpqhd.us-east-2.elasticbeanstalk.com/api/'
+const API_SERVER = 'https://hememicsapi.com/api/'
+// const API_SERVER = 'http://flask-env.eba-2cgkpqhd.us-east-2.elasticbeanstalk.com/api/'
 // const API_SERVER = 'http://10.1.10.190:5000/api/'
 
 
